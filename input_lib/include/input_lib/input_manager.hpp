6d4c33923d59529d8a4c81f0fc1f26beb754746a
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-08-19 11:51:08 -0400

    Deleted InputManager copy constructor and assignment operator.

--- input_lib/include/input_lib/input_manager.hpp
+++ input_lib/include/input_lib/input_manager.hpp
@@ -9,7 +9,10 @@ namespace InputLib
 class InputManager
 {
 public:
+    InputManager() {}
     virtual ~InputManager() {}
+    InputManager(const InputManager&) = delete;
+    void operator=(const InputManager&) = delete;
     virtual Mouse& getMouse() = 0;
     virtual Gamepad& getGamepad(unsigned index) = 0;
     virtual Keyboard& getKeyboard() = 0;
